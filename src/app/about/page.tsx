29d04387b2a4d138700ef69a6c5dad3d934c8b84
Shiying-Wu <ppriscilla0811@gmail.com>
2025-08-27 10:49:40 +1000

    update aboutpage and global css

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,11 +2,11 @@
 // layout ref: https://linear.app/changelog/2025-08-14-product-intelligence-technology-preview
 
 import {Session} from 'inspector/promises';
-// import HamburgerMenu from '../Components/HamburgerMenu';
+
 
 // <></> fragement wraps more then one element in return cluse
 
-export default function About(){
+/* export default function About(){
     return (
         <>
         <h1>How to use </h1>
@@ -19,6 +19,30 @@ export default function About(){
         </>
     )
 }
+ */
+
+
+const About=()=>{
+    return(
+        <main className="main-content">
+            <section className="centered-heading">
+                <h1>How to use</h1>
+            </section>
+
+            <section className="aligned-description">
+                <p>Developing in a team environment is hard. Coordinating Git commits, updating project boards, and managing a shared repository creates significant manual overhead that distracts from actual learning and coding.</p>
+            </section>
+
+            <section className="video-section">
+                <video width="640" height="600" controls>
+                    <source src="#" type="video/mp4" />
+                </video>
+            </section>
+        </main>
+    )
+};
+
+export default About;
 
 
 
