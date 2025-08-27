c640342ceb9a7090d4adb052718a5d41bb265c4d
Shiying-Wu <ppriscilla0811@gmail.com>
2025-08-27 18:10:34 +1000

    reupload video

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,28 +1,4 @@
-// "About Page"
-// layout ref: https://linear.app/changelog/2025-08-14-product-intelligence-technology-preview
-
-import {Session} from 'inspector/promises';
-
-
-// <></> fragement wraps more then one element in return cluse
-
-/* export default function About(){
-    return (
-        <>
-        <h1>How to use </h1>
-        <p>
-            Developing in a team environment is hard. Coordinating Git commits, updating project boards, and managing a shared repository creates significant manual overhead that distracts from actual learning and coding.
-        </p>
-        <video width="320" height="240" controls>
-            <source src="#" type="video/mp4"/>
-        </video>
-        </>
-    )
-}
- */
-
-
-const About=()=>{
+const About = () => {
     return(
         <main className="main-content">
             <section className="centered-heading">
@@ -43,9 +19,3 @@ const About=()=>{
 };
 
 export default About;
-
-
-
-
-
-
